9c2637eef1c4a006fa2547b79f88561b732ae63f
Jez <jerdavies@gmail.com>
2013-01-26 23:12:43 +0000

    Stable version with most of the comments in-place. Simulator working and tested as is the manual price point generation

---
+++ algotrading-core/src/main/java/com/lab49/algotrader/models/price/PricingWindow.java
@@ -0,0 +1,35 @@
+package com.lab49.algotrader.models.price;
+
+import com.lab49.algotrader.PricingDimensionPublisher;
+
+import java.util.List;
+
+/**
+ * The PricingWindow which accepts a publisher for the sole reason
+ * of obtaining the pricing information
+ *
+ * @author Jeremy Davies [jerdavies@gmail.com]
+ */
+public class PricingWindow implements PriceWindow {
+
+    private final PricingDimensionPublisher dimension;
+
+    /**
+     * TODO: Do we really need the publisher object here? We just
+     * want the prices
+     * @param dimension the publisher
+     */
+    public PricingWindow(PricingDimensionPublisher dimension) {
+        this.dimension = dimension;
+    }
+
+    @Override
+    public List<Price> getWindow() {
+        return dimension.getPrices();
+    }
+
+    @Override
+    public String toString() {
+        return dimension.toString();
+    }
+}
